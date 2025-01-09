2a7a8ed4c3425ba94cab56ddd6431b150cbbe9b9
ArtificialVoid <160069526+ArtificialVoid1@users.noreply.github.com>
2025-01-09 09:06:47 -0500

    Update void_wdf.h

--- wdf/void_wdf.h
+++ wdf/void_wdf.h
@@ -1,1 +1,33 @@
 
+
+
+namespace wdf {
+
+
+
+class BaseWDF {
+public:
+  float a;
+  float b;
+  float R;
+}
+
+
+
+template<typename T> 
+class Resistor : BaseWDF {
+
+
+}
+
+
+template<typename Port1type, typename Port2type>
+class SeriesAdaptor : BaseWDF {
+
+public SeriesAdaptor(Port1type Port1, Port2type Port2) {
+
+}
+
+
+}
+
