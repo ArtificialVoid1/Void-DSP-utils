c26cfaf889d6b0a43aec9cf22de5e6953dd3de81
ArtificialVoid <160069526+ArtificialVoid1@users.noreply.github.com>
2025-01-10 08:34:16 -0500

    Create Clipping.h

---
+++ DSP/Clipping.h
@@ -0,0 +1,22 @@
+#include <cmath>
+
+#define PI 3.14159
+#define TAU (2.f*PI)
+
+/**
+*Simple Hard Clipping function based on min/max and preClip Gain
+*@param Sample the input sample
+*@param Drive the gain factor
+*/
+static float BrickwallClip(float Sample, float Drive) {
+  return fmin(1.f, fmax(-1.f, Sample * Drive));
+}
+
+/**
+*Simpl Soft Clipping function based on arctan and preClip Gain
+*@param Sample the input sample
+*@param Drive the gain factor
+*/
+static float BasicSoftClip(float Sample, float Drive) {
+  return (2.f / PI) * atan(Sample * Drive);
+}
