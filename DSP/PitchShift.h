cb3a85923a2455345c37ceef7b16d6639c835518
ArtificialVoid <160069526+ArtificialVoid1@users.noreply.github.com>
2025-01-14 09:50:34 -0500

    Add Pitch Shift.h

---
+++ DSP/PitchShift.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Constants.h"
+
+//-------------------------
+
+
+//Phase Vocoder
+
+//1 Window
+//2 FFT
+//3 Split specturm
+//4 scale spectrum
+//5 phase shift spectrum
+//6 account for transients
+//7 crossfade bins
+//8 IFFT
+//9 clean data
