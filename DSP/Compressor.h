ee015c5507dc6b3862a0d1db18e874f70cf631f3
ArtificialVoid <160069526+ArtificialVoid1@users.noreply.github.com>
2025-01-30 10:06:37 -0500

    update compressor.h

--- DSP/Compressor.h
+++ DSP/Compressor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#inlcude "Envelope.h"
 
 template<typename SampleType>
 static bool Threshold(SampleType Limit, SampleType Sample) {
@@ -14,5 +15,41 @@ static SampleType BasicCompress(SampleType Sample, SampleType threshold, SampleT
   }
   else {
     return Sample;
+  }
+}
+
+template<typename SampleType>
+class Compressor {
+private:
+  SampleType Sample;
+  SampleType Threshold;
+  SampleType Ratio;
+  SampleType AttackTime;
+  SampleType ReleaseTime;
+
+  EnvType AttackType;
+  EnvType ReleaseType;
+
+public:
+  Compressor(EnvType _attackType, EnvType _releaseType) {
+    AttackType = _attackType;
+    ReleaseType = _releaseType;
+  }
+
+  inline void SetThreshold(SampleType _Threshold) {
+    Threshold = _Threshold;
+  }
+  inline void SetRatio(SampleType _Ratio) {
+    Ratio = _Ratio;
+  }
+  inline void SetAttack(float _AttackTime) {
+    AttackTime = _AttackTime;
+  }
+  inline void SetRelease(float _ReleaseTime) {
+    ReleaseTime = _ReleaseTime
+  }
+
+  inline SampleType Process(SampleType Sample) {
+    //proccess since transient with envelope
   }
 }
